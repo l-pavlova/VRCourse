cc7ddc93fd02039be09f456b190243965629a60f
Lyudmila <lusito.pav@gmail.com>
2021-03-16 12:48:16 +0200

    Add lesson two and finalized homework

---
+++ 2.ObjectsAndSimpleAnimations/Lesson 2.md
@@ -0,0 +1,37 @@
+## Objects and shapes
+
+### Animation speed - it's measured in fps(frames per second)
+- 24 fps in cinema
+- 30 fps for computer graphics
+- 60 fps for fast paced
+
+When it's in browser its the tact frequency of screen refresh
+
+stats.js measures spreed by frames per second and miliseconds per frame
+
+### Geometries:
+declaration like: 
+
+```const geom = new THREE.BoxGeometry(1,1,1);```
+
+- BoxGeometry(width : Float, height : Float, depth : Float, widthSegments : Integer, heightSegments : Integer, depthSegments : Integer) 
+- SphereGeometry(radius : Float, widthSegments : Integer, heightSegments : Integer, phiStart : Float, phiLength : Float, thetaStart : Float, thetaLength : Float)
+- CylinderGeometry(radiusTop : Float, radiusBottom : Float, height : Float, radialSegments : Integer, heightSegments : Integer, openEnded : Boolean, thetaStart : Float, thetaLength : Float)
+- TorusGeometry(radius : Float, tube : Float, radialSegments : Integer, tubularSegments : Integer, arc : Float)
+- RingGeometry(innerRadius : Float, outerRadius : Float, thetaSegments : Integer, phiSegments : Integer, thetaStart : Float, thetaLength : Float)
+- PlaneGeometry(width : Float, height : Float, widthSegments : Integer, heightSegments : Integer)//2d variation of box geometry, doesnt have a front and back side
+
+### Moving different objects together
+To move different objects together when we create a Mesh for them we add them to a group object like so:
+
+```
+const material = new THREE.MeshLambertMaterial({ color: 0x48a605 });//create a lambert material, more on materials in lesson 3
+const body = new THREE.Mesh(geom, material);
+const group = new THREE.Group();
+scene.add(group);
+group.add(body);
+```
+First we set the position like so, can be set individually or for all in the group:
+body.position.set(0,0,10);//sets xyz coordinates in the three dimensional space
+Then, we do not set the rotation of the elements one by one but of the whole group:
+ ``` group.rotation.set(0, MATH.PI/2, 0); ```
--- Homework_1/index.js
+++ Homework_1/index.js
@@ -1,11 +1,13 @@
 
 vaxInit();
-camera.position.set(50, 10, 60, 100); // Set position like this
+camera.position.set(50, 10, 60, 100);
 camera.lookAt(new THREE.Vector3(0, 0, Math.PI / 2));
 scene.background = new THREE.Color(0x485083)
+
 const bodyXPos = 0;
 const bodyYPos = 0;
 const bodyZPos = 0;
+
 const bodyGeom = new THREE.BoxGeometry(10, 10, 15);
 const headGeom = new THREE.BoxGeometry(5, 5, 5);
 const maneGeom = new THREE.SphereGeometry(6, 32, 32);
@@ -14,20 +16,20 @@ const tailTopGeom = new THREE.SphereGeometry(1, 32, 32);
 const eyeGeom = new THREE.SphereGeometry(0.5, 20, 20);
 const outerEyeGeom = new THREE.SphereGeometry(0.6, 20, 20);
 const mouthGeom = new THREE.SphereGeometry(1.2, 20, 20);
-const legGeom = new THREE.CylinderGeometry(0.75, 1.5, 5, 32, true, 0);
-const grassGeom = new THREE.BoxGeometry( 150, 150, 1 );
-
+const legGeom = new THREE.CylinderGeometry(0.75, 1.5, 5, 32);
+const grassGeom = new THREE.BoxGeometry(150, 150, 1);
+const moustacheGeom = new THREE.CylinderGeometry(0.05, 0.05, 2, 10);
 
 const grassMaterial = new THREE.MeshLambertMaterial({ color: 0x48a605 });
-const material = new THREE.MeshBasicMaterial({ color: 0xf9d71c });
-const maneMaterial = new THREE.MeshBasicMaterial({ color: 0x875223 });
-const blackMaterial = new THREE.MeshBasicMaterial({ color: 0x0f0f0f });
-const whiteMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
-const mouthMaterial = new THREE.MeshBasicMaterial({ color: 0xe0c322 });
+const material = new THREE.MeshLambertMaterial({ color: 0xf9d71c });
+const maneMaterial = new THREE.MeshLambertMaterial({ color: 0x875223 });
+const blackMaterial = new THREE.MeshLambertMaterial({ color: 0x0f0f0f });
+const whiteMaterial = new THREE.MeshLambertMaterial({ color: 0xffffff });
+const mouthMaterial = new THREE.MeshLambertMaterial({ color: 0xe0c322 });
 
 const grass = new THREE.Mesh(grassGeom, grassMaterial);
-grass.rotation.set( Math.PI/2 , 0 ,0);//(60*Math.PI);
-grass.position.set(bodyXPos - 20, bodyYPos -30, bodyZPos - 20 );//(60*Math.PI);
+grass.rotation.set(Math.PI / 2, 0, 0);//(60*Math.PI);
+grass.position.set(bodyXPos - 20, bodyYPos - 30, bodyZPos - 20);//(60*Math.PI);
 scene.add(grass);
 const bodyObj = new THREE.Group();
 scene.add(bodyObj);
@@ -36,21 +38,31 @@ const body = new THREE.Mesh(bodyGeom, material);
 body.position.set(bodyXPos, bodyYPos, bodyZPos);
 bodyObj.add(body);
 
-
 const lLeg = new THREE.Mesh(legGeom, mouthMaterial);
 const rLeg = new THREE.Mesh(legGeom, mouthMaterial);
-lLeg.position.set(bodyXPos-4, bodyYPos - 3, bodyZPos + 8 );
-rLeg.position.set(bodyXPos+4, bodyYPos - 3, bodyZPos + 8 );
+lLeg.position.set(bodyXPos - 4, bodyYPos - 3, bodyZPos + 8);
+rLeg.position.set(bodyXPos + 4, bodyYPos - 3, bodyZPos + 8);
 bodyObj.add(lLeg)
 bodyObj.add(rLeg)
 
 const lLegTop = new THREE.Mesh(tailTopGeom, mouthMaterial);
-lLegTop.position.set(bodyXPos-4, bodyYPos - 1.20, bodyZPos + 8 );
+lLegTop.position.set(bodyXPos - 4, bodyYPos - 1.20, bodyZPos + 8);
 bodyObj.add(lLegTop);
 
 const rLegTop = new THREE.Mesh(tailTopGeom, mouthMaterial);
-rLegTop.position.set(bodyXPos+ 4, bodyYPos - 1.20, bodyZPos + 8 );
+rLegTop.position.set(bodyXPos + 4, bodyYPos - 1.20, bodyZPos + 8);
 bodyObj.add(rLegTop);
+
+const lLegBack = new THREE.Mesh(legGeom, mouthMaterial);
+const rLegBack = new THREE.Mesh(legGeom, mouthMaterial);
+lLegBack.position.set(bodyXPos - 4, bodyYPos - 4, bodyZPos - 8);
+lLegBack.rotation.set(Math.PI / 2, 0, 0);
+
+rLegBack.position.set(bodyXPos + 4, bodyYPos - 4, bodyZPos - 8);
+rLegBack.rotation.set(Math.PI / 2, 0, 0);
+
+bodyObj.add(lLegBack)
+bodyObj.add(rLegBack)
 
 const headObj = new THREE.Group();
 bodyObj.add(headObj);
@@ -78,12 +90,25 @@ const mouth = new THREE.Mesh(mouthGeom, mouthMaterial);
 mouth.position.set(bodyXPos, bodyYPos + 5, bodyZPos + 12.5);
 headObj.add(mouth);
 
+for (let i = 1; i < 10; i++) {
+  const moust = new THREE.Mesh(moustacheGeom, blackMaterial);
+
+  if (i % 2 == 0) {
+    moust.position.set(bodyXPos - 1.5, bodyYPos + 5 + i / 10, bodyZPos + 13);
+    moust.rotation.set(0, 0, Math.cos(i / 10) * Math.PI / 1.5);//-(Math.PI / 2 + i * 5)
+  } else {
+    moust.position.set(bodyXPos + 1.5, bodyYPos + 5 + (i - 1) / 10, bodyZPos + 13);
+    moust.rotation.set(0, 0, - Math.cos((i - 1) / 10) * Math.PI / 1.5);
+  }
+  headObj.add(moust);
+}
+
 const nose = new THREE.Mesh(eyeGeom, blackMaterial);
 nose.position.set(bodyXPos - 0.05, bodyYPos + 5, bodyZPos + 14);
 headObj.add(nose);
 
 const mane = new THREE.Mesh(maneGeom, maneMaterial);
-mane.position.set(bodyXPos, bodyYPos + 6, bodyZPos + 5);
+mane.position.set(bodyXPos, bodyYPos + 6, bodyZPos + 6);
 headObj.add(mane);
 
 const tailObj = new THREE.Group();
@@ -93,7 +118,6 @@ const tail = new THREE.Mesh(tailGeom, material);
 tail.position.set(bodyXPos, bodyYPos + 8, bodyZPos - 8);
 const tailTop = new THREE.Mesh(tailTopGeom, maneMaterial);
 tailTop.position.set(bodyXPos, bodyYPos + 15, bodyZPos - 8);
-
 
 tailObj.add(tailTop);
 tailObj.add(tail);
@@ -105,9 +129,9 @@ lion.add(bodyObj);
 lion.add(headObj);
 
 function animate() {
- 
+
   const rotationPos = Math.sin(Date.now() * 0.001) * Math.PI / 2 * 0.5
   tailObj.rotation.z = rotationPos;
 
-  lion.rotation.set(0, t/3, 0);
+  lion.rotation.set(0, t / 3, 0);
 }
